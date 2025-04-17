397494aeb313ee08c02470afe1949cf552c2772a
Eduardo Nienkotter Marcelino <110943998+dudunm@users.noreply.github.com>
2025-04-17 19:28:27 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/exercicio21/Exercicio21.java
@@ -0,0 +1,10 @@
+package com.mycompany.exercicio21;
+public class Exercicio21 {
+    public static void main(String[] args) {
+        for(int num = 33; num < 57; num++ ){
+            if(num % 2 == 0){
+            System.out.println(num);
+            }
+        } 
+    }
+}
